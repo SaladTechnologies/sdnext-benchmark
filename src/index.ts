77fd97bdb54b6ad08713f9675f00ab014471f5ff
Shawn <shawn.rushefsky@gmail.com>
2023-08-07 12:08:36 -0400

    init comit

---
+++ src/index.ts
@@ -0,0 +1,97 @@
+import { Text2ImageRequest, Text2ImageResponse, ServerStatus } from "./types";
+import fs from "node:fs/promises";
+import path from "node:path";
+
+const { SDNEXT_URL = "http://localhost:7860", OUTPUT_DIR="images", BENCHMARK_SIZE = "10" } = process.env;
+
+
+
+const benchmarkSize = parseInt(BENCHMARK_SIZE, 10);
+
+async function getJob(): Promise<Text2ImageRequest> {
+  return {
+    prompt: "cat",
+    steps: 35,
+    width: 1216,
+    height: 896,
+    send_images: true,
+    cfg_scale: .7
+  };
+}
+
+async function submitJob(job: Text2ImageRequest): Promise<Text2ImageResponse> {
+  // POST to SDNEXT_URL
+  const url = new URL("/sdapi/v1/txt2img", SDNEXT_URL);
+  const response = await fetch(url.toString(), {
+    method: "POST", 
+    body: JSON.stringify(job),
+    headers: {
+      "Content-Type": "application/json"
+    }
+  });
+
+  const json = await response.json();
+  return json as Text2ImageResponse;
+}
+
+let numImages = 0;
+async function uploadImage(image: string): Promise<string> {
+  const filename = path.join(OUTPUT_DIR, `image-${numImages++}.jpg`);
+  await fs.writeFile(filename, Buffer.from(image, "base64"));
+  return filename;
+}
+
+async function getServerStatus(): Promise<ServerStatus> {
+  const url = new URL("/sdapi/v1/system-info/status?state=true&memory=true&full=true&refresh=true", SDNEXT_URL);
+  const response = await fetch(url.toString());
+  const json = await response.json();
+  return json as ServerStatus;
+}
+
+async function sleep(ms: number): Promise<void> {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+let stayAlive = true;
+process.on("SIGINT", () => {
+  stayAlive = false;
+});
+
+async function waitForServerToStart(): Promise<void> {
+  while (stayAlive) {
+    try {
+      await getServerStatus();
+      return;
+    } catch (e) {
+      console.log("Waiting for server to start...");
+      await sleep(1000);
+    }
+  }
+}
+
+const prettyPrint = (obj: any): void => console.log(JSON.stringify(obj, null, 2));
+
+async function main(): Promise<void> {
+
+  await fs.mkdir(OUTPUT_DIR, { recursive: true });
+  await waitForServerToStart();
+
+  let numImages = 0;
+  const start = Date.now();
+  while (stayAlive && numImages < benchmarkSize) {
+    const job = await getJob();
+    const response = await submitJob(job);
+    numImages += response.images.length;
+
+    Promise.all(response.images.map(uploadImage)).then((filenames) => {
+      console.log(filenames);
+    });
+  }
+
+  const end = Date.now();
+  const elapsed = end - start;
+  console.log(`Generated ${numImages} images in ${elapsed}ms`);
+  console.log(`Average time per image: ${elapsed / numImages}ms`);
+}
+
+main();
---
+++ src/types.ts
@@ -0,0 +1,163 @@
+export interface Text2ImageRequest {
+  enable_hr?: boolean;
+  denoising_strength?: number;
+  firstphase_width?: number;
+  firstphase_height?: number;
+  hr_scale?: number;
+  hr_upscaler?: string;
+  hr_second_pass_steps?: number;
+  hr_resize_x?: number;
+  hr_resize_y?: number;
+  refiner_start?: number;
+  refiner_prompt?: string;
+  refiner_negative?: string;
+  prompt: string;
+  styles?: string[];
+  seed?: number;
+  subseed?: number;
+  subseed_strength?: number;
+  seed_resize_from_h?: number;
+  seed_resize_from_w?: number;
+  sampler_name?: string;
+  latent_sampler?: string;
+  batch_size?: number;
+  n_iter?: number;
+  steps: number;
+  cfg_scale: number;
+  image_cfg_scale?: number;
+  clip_skip?: number;
+  width: number;
+  height: number;
+  restore_faces?: boolean;
+  tiling?: boolean;
+  do_not_save_samples?: boolean;
+  do_not_save_grid?: boolean;
+  negative_prompt?: string;
+  eta?: number;
+  diffusers_guidance_rescale?: number;
+  s_min_uncond?: number;
+  s_churn?: number;
+  s_tmax?: number;
+  s_tmin?: number;
+  s_noise?: number;
+  override_settings?: OverrideSettings;
+  override_settings_restore_afterwards?: boolean;
+  script_args?: string[];
+  sampler_index?: string;
+  script_name?: string;
+  send_images: boolean;
+  save_images?: boolean;
+  alwayson_scripts?: AlwaysonScripts;
+}
+
+export interface OverrideSettings {}
+
+export interface AlwaysonScripts {}
+
+export interface Text2ImageResponse {
+  images: string[];
+  parameters: Text2ImageRequest;
+  info: string;
+}
+
+export interface ServerStatus {
+  version: Version
+  uptime: string
+  timestamp: string
+  state: State
+  memory: Memory
+  platform: Platform
+  torch: string
+  gpu: Gpu2
+  optimizations: string[]
+  crossatention: string
+  device: Device
+  backend: string
+  pipeline: string
+}
+
+export interface Version {
+  app: string
+  updated: string
+  hash: string
+  url: string
+}
+
+export interface State {
+  started: string
+  step: string
+  jobs: string
+  flags: string
+  job: string
+  "text-info": string | null
+}
+
+export interface Memory {
+  ram: Ram
+  gpu: Gpu
+  "gpu-active": GpuActive
+  "gpu-allocated": GpuAllocated
+  "gpu-reserved": GpuReserved
+  "gpu-inactive": GpuInactive
+  events: Events
+  utilization: number
+}
+
+export interface Ram {
+  free: number
+  used: number
+  total: number
+}
+
+export interface Gpu {
+  free: number
+  used: number
+  total: number
+}
+
+export interface GpuActive {
+  current: number
+  peak: number
+}
+
+export interface GpuAllocated {
+  current: number
+  peak: number
+}
+
+export interface GpuReserved {
+  current: number
+  peak: number
+}
+
+export interface GpuInactive {
+  current: number
+  peak: number
+}
+
+export interface Events {
+  retries: number
+  oom: number
+}
+
+export interface Platform {
+  arch: string
+  cpu: string
+  system: string
+  release: string
+  python: string
+}
+
+export interface Gpu2 {
+  device: string
+  cuda: string
+  cudnn: number
+  driver: string
+}
+
+export interface Device {
+  active: string
+  dtype: string
+  vae: string
+  unet: string
+}
